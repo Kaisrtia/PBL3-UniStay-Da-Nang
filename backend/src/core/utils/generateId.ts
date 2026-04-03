72231068b1754b608d38b0bcc0217513248c43c8
Kaisrtia <nqhtrung304@gmail.com>
2026-04-03 17:22:56 +0700

    upd: changed logic for generating Id

--- backend/src/core/utils/generateId.ts
+++ backend/src/core/utils/generateId.ts
@@ -1,14 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+import { nanoid } from "nanoid";
 
 const prisma = new PrismaClient();
 
-// generate id for user
-export const generateUserId = (role: string) => {
-  if (role === 'ADMIN') {
-    return 'AD_' + (Number(prisma.user.count()) + 1).toString();
-  } else if (role === 'STUDENT') {
-    return 'S_' + (Number(prisma.user.count()) + 1).toString();
-  } else if (role === 'HOST') {
-    return 'H_' + (Number(prisma.user.count()) + 1).toString();
-  }
-};
+export const generateHybridId = (prefix: string) => {
+  return prefix + nanoid();
+}
