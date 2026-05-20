223629f63d215fe8cd388902d2708c7eff64c486
Kaisrtia <nqhtrung304@gmail.com>
2026-05-20 22:25:35 +0700

    fix(core module): authorize any allowed user role

--- backend/src/core/middlewares/role.middleware.ts
+++ backend/src/core/middlewares/role.middleware.ts
@@ -4,7 +4,10 @@ import HttpStatus from 'http-status';
 
 export const authorize = (allowedRoles: account_role[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    if (!req.user || !req.user.roles.includes(allowedRoles[0])) {
+    const hasAllowedRole =
+      req.user?.roles.some((role) => allowedRoles.includes(role)) ?? false;
+
+    if (!hasAllowedRole) {
       return res.status(HttpStatus.FORBIDDEN).json({
         message: 'You do not have permission to perform this action!'
       });
